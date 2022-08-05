1ad654ece1eb44b095a9a0db3c1983a58f7555d2
CesarGBkR <yexotortuesprin@gmail.com>
2022-08-05 13:43:08 -0500

    Adding post form test

---
+++ form-post/server.php
@@ -0,0 +1,10 @@
+<?php
+
+    echo "<pre>";
+    var_dump($_POST);
+    echo "</pre>";
+
+    // $name = $_GET["name"];
+    // $age = $_GET["age"];
+
+    // echo "The user $name has $age years old";
