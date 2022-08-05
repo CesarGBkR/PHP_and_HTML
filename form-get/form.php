0bf08b6dc0c9f61dbcf6e9e6eaa5a5e0315bb749
CesarGBkR <yexotortuesprin@gmail.com>
2022-08-05 13:38:26 -0500

    Adding get form test

---
+++ form-get/form.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form from GET</title>
+</head>
+<body>
+    <form action="./server.php" method="get">
+
+        <label for="name"></label>
+        <input type="text" name="name">
+
+        <label for="age"></label>
+        <input type="text" name="age">
+
+        <button type="submit">Send form</button>
+
+    </form>
+</body>
+</html>
